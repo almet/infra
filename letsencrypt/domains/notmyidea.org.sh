bbad0dbb77a88f015d80bc7cd4578420cf1dea56
Alexis Métaireau <alexis@notmyidea.org>
2016-03-25 22:45:06 +0100

    Add photos and gogs

--- letsencrypt/domains/notmyidea.org.sh
+++ letsencrypt/domains/notmyidea.org.sh
@@ -3,5 +3,6 @@
 /home/poils/letsencrypt/letsencrypt-auto certonly --renew-by-default --webroot -w /home/www/letsencrypt-requests/ \
 -d blog.notmyidea.org \
 -d shout.notmyidea.org \
--d gitlab.notmyidea.org \
--d pad.notmyidea.org
+-d pad.notmyidea.org \
+-d git.notmyidea.org \
+-d photos.notmyidea.org
