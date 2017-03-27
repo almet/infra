e5bb7c9f209e9c4f8d76531b529abe584b12abad
Alexis Métaireau <alexis@notmyidea.org>
2017-03-27 14:16:22 +0200

    Remove www from kinto-storage.org.

--- letsencrypt/domains/kinto-storage.org.sh
+++ letsencrypt/domains/kinto-storage.org.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 /home/poils/letsencrypt/letsencrypt-auto certonly --renew-by-default --webroot -w /home/www/letsencrypt-requests/ \
--d slack.kinto-storage.org \
--d www.kinto-storage.org
+-d slack.kinto-storage.org
 
