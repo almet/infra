fa3c8374eab5176e5e56c1b6fc2e65837762a7b6
Alexis Métaireau <alexis@notmyidea.org>
2017-04-18 17:55:34 +0200

    Fix SSL for reseaugrappe.org

--- letsencrypt/domains/reseaugrappe.org.sh
+++ letsencrypt/domains/reseaugrappe.org.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 /home/poils/letsencrypt/letsencrypt-auto certonly --renew-by-default --webroot -w /home/www/letsencrypt-requests/ \
--d board.reseaugrappe.org \
 -d forum.reseaugrappe.org \
 -d cloud.reseaugrappe.org
